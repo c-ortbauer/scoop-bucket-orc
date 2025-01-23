5a2150c724d4527f092eb0c89ae58db6b071e44c
c-ortbauer <111966242+c-ortbauer@users.noreply.github.com>
2025-01-23 08:10:34 +0100

    Update user/bucketname in auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "c-ortbauer/scoop-bucket-orc:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
